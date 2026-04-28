4f6d4b7279a6a24be329606c41878681a741e83e
rateelbahathiq <rateelbahathiq@gmail.com>
2026-04-28 06:28:34 +0000

    fix error

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 import sys
 
-if len(sys.argv) < 2:
-    print("Usage: ./0-fizzbuzz.py <number>")
-    sys.exit(1)
-
 n = int(sys.argv[1])
 
 for i in range(1, n + 1):
     if i % 15 == 0:
-        print("FizzBuzz", end=" ")
+        print("FizzBuzz", end="")
     elif i % 3 == 0:
-        print("Fizz", end=" ")
+        print("Fizz", end="")
     elif i % 5 == 0:
-        print("Buzz", end=" ")
+        print("Buzz", end="")
     else:
-        print(i, end=" ")
+        print(i, end="")
+
+    if i != n:
+        print(" ", end="")
 
 print()
